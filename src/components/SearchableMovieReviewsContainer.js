56d14f8d72203a5c7d3868e0b55452298782de88
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-01-28 23:30:03 +0000

    Automatically backed up by Learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -3,7 +3,7 @@ import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
 const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
+const URL = `https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${query}`
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
@@ -20,6 +20,10 @@ export default class LatestMovieReviewsContainer extends Component {
     this.setState({
       query: query
     })
+  }
+
+  fetchReviews = (query) => {
+    fetch(URL + query)
   }
   render() {
     return(
