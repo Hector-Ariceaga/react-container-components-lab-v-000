4b07b33e9d0adef0b1cbe7dc5b2be49d96459cee
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-01-28 23:31:39 +0000

    Automatically backed up by Learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -30,8 +30,8 @@ export default class LatestMovieReviewsContainer extends Component {
   render() {
     return(
       <form onSubmit={this.fetchReviews}>
-        <input type="text" value={this.state.query} onChange={this.handleChange}>
-        <input type="submit" value="Search">
+        <input type="text" value={this.state.query} onChange={this.handleChange} />
+        <input type="submit" value="Search" />
       </form>
       <div className="searchable-movie-reviews">
         <MovieReviews reviews={this.state.reviews}/>
