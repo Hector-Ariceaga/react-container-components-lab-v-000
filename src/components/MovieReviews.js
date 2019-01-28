27eabc5b881fcde3652a51c153841434c29c939e
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-01-28 22:56:05 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -5,7 +5,7 @@ const MovieReviews = ({reviews}) => {
   return(
     <div className='review-list'>
     {console.log(reviews)}
-      {reviews.map(review => <Review />)}
+      {reviews.map(review => <Review movieTitle={display_title} reviewTitle={headline} author={byline} url={link.url}/> )}
     </div>
   )
 }
