69d4acc6ae49e99ddf35ba384e422ff19836033a
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-01-28 22:23:01 +0000

    Automatically backed up by Learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,15 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+export default class LatestMovieReviewsContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      reviews = []
+    }
+  }
+
+  
+
+  
+}
